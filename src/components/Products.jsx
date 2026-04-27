bcc1d294ce1fe3cdc314b94c5fc252b7d55d00d9
DevoloperYigidoo <yigitefe9900@gmail.com>
2026-04-27 12:53:01 +0300

    responsive özellik eklendi

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,40 +4,49 @@ import CardMedia from "@mui/material/CardMedia"
 import Typography from "@mui/material/Typography"
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
-import { useNavigate} from "react-router-dom"
-
-
+import { useNavigate } from "react-router-dom"
 
 function Products({ product }) {
-
-
     const navigate = useNavigate()
 
     const setSelectedProduct = () => {
         navigate(`product-details/${product.id}`);
     }
-    
+
     return (
-        <div className="product-card" >
-            <Card sx={{ width:370,height:330,display:"flex" ,flexDirection:"column", justifyContent:"space-between"}}>
+        <div className="product-card">
+            <Card sx={{
+                width: { xs: "90vw", sm: 320, md: 370 },
+                height: { xs: "auto", sm: 330 },
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between"
+            }}>
                 <CardMedia
-                    sx={{ height: 140, width: 140,margin:"0px 115px" } }
+                    sx={{
+                        height: 140,
+                        width: 140,
+                        margin: "0 auto",
+                        mt: 1
+                    }}
                     image={product.image}
                 />
                 <CardContent>
-                    <Typography gutterBottom  >
+                    <Typography gutterBottom>
                         {product.title}
                     </Typography>
-
-                    <Typography gutterBottom sx={{fontSize:10}}  >
-                        {product.description.length <= 250 ? product.description : null }
+                    <Typography gutterBottom sx={{ fontSize: 10 }}>
+                        {product.description.length <= 250 ? product.description : null}
                     </Typography>
-                    <div style={{color:"red"}}>{product.price}₺</div> 
+                    <div style={{ color: "red" }}>{product.price}₺</div>
                 </CardContent>
-
-                <CardActions >
-                    <Button variant="contained" color="primary" size="small" onClick={setSelectedProduct}>Detayları Göster</Button>
-                    <Button variant="contained" color="success" size="small">Satın Al</Button>
+                <CardActions>
+                    <Button variant="contained" color="primary" size="small" onClick={setSelectedProduct}>
+                        Detayları Göster
+                    </Button>
+                    <Button variant="contained" color="success" size="small">
+                        Satın Al
+                    </Button>
                 </CardActions>
             </Card>
         </div>
